e06ad7cc87c9cb9e4bc1de6dfe55c39226d91782
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2023-12-24 10:25:36 +0700

    update and adding files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".title",{
+    strings : ["もしもし", "Hello", "I am Fahrur"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    string : ["もしもし", "Halo", "I am Fahrur"],
+    typespeed : 150,
+    backspeed : 150,
+    looped : true
+})
