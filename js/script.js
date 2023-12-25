25775baa9c1ae6fe748d2a1bee2c47e2cd9fd555
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2023-12-25 12:09:08 +0700

    update files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(section => { 
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    })
+}
+
 var typed = new Typed(".title",{
     strings : ["もしもし", "Hello!", "I am Fahrur"],
     typeSpeed : 150,
