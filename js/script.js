af6d8f727245a2e6c1822664e3c024d1eee39db0
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-05-02 00:37:57 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 let sections = document.querySelectorAll('section');
 
 window.onscroll = () => {
-    sections.forEach(section => { 
+    sections.forEach(section => {
         let top = window.scrollY;
         let offset = section.offsetTop;
         let height = section.offsetHeight;
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             section.classList.add('active');
         } else {
             section.classList.remove('active');
         }
 
-        if(window.scrollY + window.innerHeight >= document.body.scrollHeight){
+        // when end of page show footer
+        console.log('scrollY:', window.scrollY);
+        console.log('innerHeight:', window.innerHeight);
+        console.log('scrollHeight:', document.body.scrollHeight);
+
+        if (window.scrollY + window.innerHeight <= document.body.scrollHeight) {
             document.querySelector('.sec-footer').style.opacity = 1;
         } else {
             document.querySelector('.sec-footer').style.opacity = 0;
@@ -20,9 +25,9 @@ window.onscroll = () => {
     })
 }
 
-var typed = new Typed(".title",{
-    strings : ["もしもし", "Hello!", "I am Fahrur"],
-    typeSpeed : 150,
-    backSpeed : 150,
-    loop : true
+var typed = new Typed(".title", {
+    strings: ["もしもし", "Hello!", "I am Fahrur"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
 })
