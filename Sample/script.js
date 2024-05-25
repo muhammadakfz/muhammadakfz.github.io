eda8779f6f61b7caab512ac4bf1158565ae9c634
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-05-25 14:51:05 +0700

    delete all files

--- Sample/script.js
+++
@@ -1,9 +0,0 @@
-function showNotification() {
-    var notification = document.getElementById("notification");
-    notification.style.display = "block";
-}
-
-function closeNotification() {
-    var notification = document.getElementById("notification");
-    notification.style.display = "none";
-}
