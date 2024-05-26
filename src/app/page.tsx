ab0b3edf36baa9bd83247fee703a3554320e2226
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-05-26 16:42:11 +0700

    update description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
           <Image src="/pp.jpg" alt="Picture of the author" width={400} height={400} className='rounded-full border-[20px] border-solid border-white drop-shadow-2xl basis-1/4 rounded-full justify-end' />
           <div className='flex flex-col pl-[50px]'>
             <h1 className='font-semibold text-4xl'>About Me👨‍💻</h1>
-            <p className='pt-[50px] max-w-[600px]'>Muhammad Akhyar Fahrurrozi, yang akrab dipanggil Fahrur, adalah seorang programmer yang memulai karirnya sejak 2019. Sebagai lulusan Dicoding Academy, ia memiliki minat mendalam dalam kompetitif programming, pengembangan web, machine learning, dan hacking. Fahrur telah menciptakan berbagai aplikasi web inovatif dan mengembangkan model machine learning yang cerdas. Meskipun telah mencapai banyak hal, Fahrur menyadari bahwa ia masih perlu banyak belajar. Dengan dedikasi untuk terus berkembang, ia selalu berusaha memberikan kontribusi terbaik dalam setiap proyek yang ia kerjakan.</p>
+            <p className='pt-[50px] max-w-[600px]'>My name is Muhammad Akhyar Fahrurrozi, usually called Fahrur, I am a programmer who started my career since 2019. As a graduate of Dicoding Academy, I have a deep interest in competitive programming, web development, machine learning, and hacking. I have studied a lot of innovative web application development and developed intelligent machine learning models. Although I have achieved a lot, I realize that I still have a lot to learn. With dedication and passion for continuous improvement, I always try to give my best contribution in every project I work on.</p>
           </div>
         </div>
       </div>
