e7ec87e97fbbb2a0775b3e8281aca2a28554f748
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-05-26 16:48:23 +0700

    add transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,9 +46,9 @@ export default function Home() {
           </h1>
         </div>
       </div>
-      <div id="about" className="h-[100vh] flex justify-center items-center">
+      <div id="about" className="h-[100vh] flex justify-center items-center" data-aos="fade-up">
         <div className='flex flex-row place-content-center content-center place-items-center'>
-          <Image src="/pp.jpg" alt="Picture of the author" width={400} height={400} className='rounded-full border-[20px] border-solid border-white drop-shadow-2xl basis-1/4 rounded-full justify-end' />
+          <Image src="/pp.jpg" alt="Picture of the author" data-aos="fade-up" width={400} height={400} className='rounded-full border-[20px] border-solid border-white drop-shadow-2xl basis-1/4 rounded-full justify-end' />
           <div className='flex flex-col pl-[50px]'>
             <h1 className='font-semibold text-4xl'>About Me👨‍💻</h1>
             <p className='pt-[50px] max-w-[600px]'>My name is Muhammad Akhyar Fahrurrozi, usually called Fahrur, I am a programmer who started my career since 2019. As a graduate of Dicoding Academy, I have a deep interest in competitive programming, web development, machine learning, and hacking. I have studied a lot of innovative web application development and developed intelligent machine learning models. Although I have achieved a lot, I realize that I still have a lot to learn. With dedication and passion for continuous improvement, I always try to give my best contribution in every project I work on.</p>
