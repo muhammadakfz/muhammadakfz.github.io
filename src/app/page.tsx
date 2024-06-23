e5104aff32c5ba5dcace0ee6b3b0098df3006bc2
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-06-23 22:59:23 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,29 @@ import Image from 'next/image'
 
 export default function Home() {
 
+  // text animation
+  const textRef = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(textRef.current, {
+      strings: ["web developer ", "competitive programming ", "machine learning enthusiast "],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+      showCursor: true,
+      cursorChar: "|",
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <>
       <title>muhammadakfz | Hello!</title>
       <body className="max-w-2xl mb-40 md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0"> 
-          <h1 className="mb-8 text-2xl font-medium tracking-tight ">Hi! I am Fahrur ✌️</h1>
+          <h1 className="text-2xl font-medium tracking-tight ">Hi! I am Fahrur <span className="wave">👋</span></h1>
           <p className="text-lg mt-4 prose prose-neutral dark:prose-invert">
             I am a programmer who started my career since 2019. As a graduate of Dicoding Academy, I have a deep interest in competitive programming, web development, machine learning, and hacking.
           </p>
