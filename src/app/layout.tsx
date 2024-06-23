02a9c80dc8d651fd366217a2a6d5abb59cb36d36
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-06-23 23:29:23 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { GeistSans } from 'geist/font/sans';
 import "./styles/globals.css";
 import Link from "next/link";
 
-const inter = GeistSans;
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,13 +22,13 @@ export default function RootLayout({
   );
 
   return (
-    <html lang="en">
+    <html lang="en" className={GeistSans.className}>
       <body>
-        <aside className="-ml-[8px] mb-16 tracking-tight">
+        <aside className="-ml-[8px] mt-10 mb-16 tracking-tight">
           <div className="lg:sticky lg:top-20">
-            <nav className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative" id="nav">
+              <nav className={`flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative ${GeistSans.className}`} id="nav">
               <div className="flex flex-row space-x-0 pr-10">
-                <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2" href="/">home</a>
+                <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 " href="/">home</a>
                 <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2" href="#work">about</a>
                 <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2" href="https://github.com/muhammadakfz">github</a>
                 <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2" href="#contact">contact me</a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,33 +7,19 @@ import Typed from 'typed.js'
 import styles from "./styles/Home.module.css";
 import "./styles/globals.css";
 import Image from 'next/image'
+import { GeistSans } from 'geist/font/sans';
+
 
 export default function Home() {
 
-  // text animation
-  const textRef = useRef(null);
-  useEffect(() => {
-    const typed = new Typed(textRef.current, {
-      strings: ["web developer ", "competitive programming ", "machine learning enthusiast "],
-      typeSpeed: 50,
-      backSpeed: 50,
-      loop: true,
-      showCursor: true,
-      cursorChar: "|",
-    });
-
-    return () => {
-      typed.destroy();
-    };
-  }, []);
 
   return (
     <>
       <title>muhammadakfz | Hello!</title>
-      <body className="max-w-2xl mb-40 md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0"> 
-          <h1 className="text-2xl font-medium tracking-tight ">Hi! I am Fahrur <span className="wave">👋</span></h1>
-          <p className="text-lg mt-4 prose prose-neutral dark:prose-invert">
+      <body className={'max-w-2xl mb-40 md:flex-row mx-4 mt-8 lg:mx-auto ${GeistSans.className}'}>
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 ">
+          <h1 className={`text-2xl mb-10 antialiased font-medium tracking-tight ${GeistSans.className}`}>Hi! l am Fahrur <span className="wave">👋</span></h1>
+          <p className={`text-lg mt-4 prose prose-neutral dark:prose-invert ${GeistSans.className}`}>
             I am a programmer who started my career since 2019. As a graduate of Dicoding Academy, I have a deep interest in competitive programming, web development, machine learning, and hacking.
           </p>
         </main>
