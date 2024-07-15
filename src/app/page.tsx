5687e80a0272bcdb94086d5809978110aba94b25
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-07-16 06:54:29 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,42 +6,60 @@ import 'aos/dist/aos.css';
 import "./styles/globals.css"
 import { GeistSans } from 'geist/font/sans';
 
+const SocialMediaButton = ({ url, icon, altText, className }) => {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer" className="social-media-button">
+      <img src={icon} alt={altText} className={`icon ${className}`} />
+    </a>
+  );
+};
+
+// Example usage of SocialMediaButton
+const SocialMediaLinks = () => {
+  return (
+    <div className="social-media-links mt-10 flex flex-grid">
+      <SocialMediaButton
+        url="https://twitter.com/yourusername"
+        icon="/instagram.png"
+        altText="Instagram"
+        className={"w-8 h-8 mr-5"}
+      />
+      <SocialMediaButton
+        url="https://linkedin.com/in/yourusername"
+        icon="/x.png"
+        altText="X"
+        className={"w-7 h-7 mr-5"}
+      />
+      <SocialMediaButton
+        url="https://github.com/muhammadakfz"
+        icon="/github.png"
+        altText="Github"
+        className={"w-8 h-8"}
+      />
+    </div>
+  );
+};
+
 
 export default function Home() {
-
-
   return (
     <>
       <title>muhammadakfz✌️</title>
-      <body className={'max-w-2xl mb-40 md:flex-row mx-4 mt-8 lg:mx-auto ${GeistSans.className}'}>
+      <body className={`max-w-2xl mb-40 md:flex-row mx-4 mt-8 lg:mx-auto ${GeistSans.className}`}>
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 ">
           <h1 className={`text-2xl mb-10 antialiased font-medium tracking-tight ${GeistSans.className}`}>Hi! l am Fahrur <span className="wave">👋</span></h1>
           <p className={`text-lg mt-4 prose prose-neutral dark:prose-invert ${GeistSans.className}`}> I'm a programmer with a strong passion for competitive programming, web development, machine learning, and cybersecurity. Since starting my career in 2019, I have been committed to continuous learning and growth.</p>
-          <p className={`text-lg mt-4 prose prose-neutral dark:prose-invert ${GeistSans.className}`}>
-            Currently, I am freelancing as a web developer, data analyst, and few other things. I am a student at <a href="https://www.itb.ac.id/" target="_blank" className="text-blue-500">Institut Teknologi Bandung</a>, majoring in Informatics Engineering.
-          </p>
           {/* <div className="mt-5">
             <a href="/muhammadakfz_CV.pdf" download className="bg-blue-700 text-white text-small font-semibold px-4 py-2 rounded-md">Download CV</a>
           </div> */}
-
-          <h2 className={`text-2xl mt-10 mb-4 antialiased font-medium tracking-tight ${GeistSans.className}`}>My Skills</h2>
+    
+          <SocialMediaLinks/>
           
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
-            <div className="p-4 bg-indigo-600 rounded-md">
-              <h3 className={`text-xl font-medium ${GeistSans.className}`}>Web Development</h3>
-              <p className={`text-lg ${GeistSans.className}`}>React, Next.js, TailwindCSS, Node.js, Express.js, MongoDB, PostgreSQL</p>
-            </div>
-            <div className="p-4 bg-indigo-600 rounded-md">
-              <h3 className={`text-xl font-medium ${GeistSans.className}`}>Machine Learning</h3>
-              <p className={`text-lg ${GeistSans.className}`}>Python, TensorFlow, Keras, Scikit-learn, Pandas, Numpy</p>
-            </div>
-            <div className="p-4 bg-indigo-600 rounded-md">
-              <h3 className={`text-xl font-medium ${GeistSans.className}`}>Game Development</h3>
-              <p className={`text-lg ${GeistSans.className}`}>Unity, C#, C++</p>
-            </div>
+          <div className="mt-10">
+            <a href="#work" className="bg-black text-white text-small font-semibold px-4 py-3 rounded-md btn">learn more</a>
           </div>
         </main>
       </body>
-    </>
-  )
+    </> 
+  ) 
 }
