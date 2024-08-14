04d182b14c0bf6be91aeec58ac9f528f0b129225
Muhammad Akhyar Fahrurrozi <89767916+muhammadakfz@users.noreply.github.com>
2024-08-14 22:07:07 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,12 @@ const SocialMediaLinks = () => {
         altText="Github"
         className={"w-8 h-8"}
       />
+      <SocialMediaButton
+        url='https://www.linkedin.com/in/muhammadakfz/'
+        icon='/linkedin.png'
+        altText='LinkedIn'
+        className='w-8 h-8 ml-5'
+      />
     </div>
   );
 };
@@ -48,18 +54,22 @@ export default function Home() {
       <body className={`max-w-2xl mb-40 md:flex-row mx-4 mt-8 lg:mx-auto ${GeistSans.className}`}>
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 ">
           <h1 className={`text-2xl mb-10 antialiased font-medium tracking-tight ${GeistSans.className}`}>Hi! l am Fahrur <span className="wave">👋</span></h1>
-          <p className={`text-lg mt-4 prose prose-neutral dark:prose-invert ${GeistSans.className}`}> I'm a programmer with a strong passion for competitive programming, web development, machine learning, and cybersecurity. Since starting my career in 2019, I have been committed to continuous learning and growth.</p>
+          <p className={`text-lg mt-4 prose prose-neutral dark:prose-invert ${GeistSans.className}`}>
+            Hello everyone, I'm Fahrur, a physics student at the University of Indonesia. I'm passionate about the intersection of physics and technology and strive to create innovative solutions. Through self-learning, I'm eager to contribute to projects that combine my passions.  I am committed to learning and growing in this field.
+          </p>
           {/* <div className="mt-5">
             <a href="/muhammadakfz_CV.pdf" download className="bg-blue-700 text-white text-small font-semibold px-4 py-2 rounded-md">Download CV</a>
           </div> */}
-    
-          <SocialMediaLinks/>
-          
+
+          <SocialMediaLinks />
+
           <div className="mt-10">
             <a href="/about" className="bg-black text-white text-small font-semibold px-4 py-3 rounded-md btn">learn more</a>
           </div>
+
+          
         </main>
       </body>
-    </> 
-  ) 
+    </>
+  )
 }
