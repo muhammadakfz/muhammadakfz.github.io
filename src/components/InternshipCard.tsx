2848a6be511088d569fde6e3f6a966c46f7ae0b7
muhammadakfz <muhammadakfz@gmail.com>
2025-08-20 21:42:11 +0700

    update

--- src/components/InternshipCard.tsx
+++ src/components/InternshipCard.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 
 const InternshipCard = () => {
   const openMail = () => {
-    window.open('mailto:raphaelgiraud12@gmail.com', '_blank');
+    window.open('mailto:muhammadakfz@gmail.com', '_blank');
   };
   const router = useRouter();
 
